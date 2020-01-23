1f9c749ec268c672492269694a3247fc5df9f729
Junaid Rahim <junaidrahim8d@gmail.com>
2020-01-23 21:55:09 +0530

    added testing boilerplate

---
+++ src/__tests__/Login.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, fireEvent, waitForElement } from '@testing-library/react'
+
+import Login from '../Components/Login/Login'
+
+describe("Login Component", ()=>{
+    test("Should display a blank login form", async ()=>{
+        // ??
+    })
+})
